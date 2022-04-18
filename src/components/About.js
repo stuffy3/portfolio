04d4e6e549b57395c71d13f888e9a5112de42de1
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-04-17 21:21:23 -0600

    added about page

--- src/components/About.js
+++ src/components/About.js
@@ -45,7 +45,21 @@ function About() {
                         position: 'top'
                     }}
                     />
+                    <div className='Small-Rect-Container-Text'>
+                        
+                        I've been in the business of creating ever since I could climb up on a piano bench and charismatically mash 
+                        the keys of a piano. That charisma and love of creating has taken me to pursue music composition, art and now, 
+                        web development. Since my decision to attend and graduate from the coding bootcamp, DevMountain, 
+                        I've been obsessed with learning and creating things using JavaScript, HTML and CSS. When I'm not learning or creating,
+                        I'm probably hanging out with my wife, watching the stock market or messing with a synthesizer.
+                            
+                    </div>
                 </div>
+            </div>
+        </div>
+        <div className='About-Main-Content-Right'>
+            <div className='About-Inner-Content-Right'>
+                   <h1>Here's what I know...</h1>
             </div>
         </div>
     </div>
